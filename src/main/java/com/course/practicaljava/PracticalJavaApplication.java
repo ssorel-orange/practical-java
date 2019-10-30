7a977bced4ee5dbbe502c8c35876fa87c511e069
ssorel-orange <stephane.sorel@orange.com>
2019-10-30 15:10:38 -0400

    Initial commit

---
+++ src/main/java/com/course/practicaljava/PracticalJavaApplication.java
@@ -0,0 +1,17 @@
+package com.course.practicaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PracticalJavaApplication  {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalJavaApplication.class, args);
+	}
+
+}
