0e9fecc287a58084366cda1ef472255c4f7233e6
ssorel-orange <stephane.sorel@orange.com>
2019-11-19 00:58:57 -0400

    Working with headers
    Added API to demonstrate headers manipulation

--- src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
+++ src/main/java/com/course/practicaljava/rest/controller/DefaultRestController.java
@@ -4,11 +4,16 @@
 package com.course.practicaljava.rest.controller;
 
 import java.time.LocalTime;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +38,25 @@ public class DefaultRestController {
 	public String time( ) {
 		return LocalTime.now().toString();
 	}
-	
+	@GetMapping (path ="/header-one")
+	public String headerByAnnotation(@RequestHeader(name = "User-agent") String headerUserAgent, @RequestHeader(name= "Practical-java", required = false) String headerPracticalJava) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("User-agent : " + headerUserAgent);
+		sb.append(" || ");
+		sb.append("Practical-java : " + headerPracticalJava);
+		return sb.toString();
+		
+	}
+	@GetMapping (path ="/header-two")
+	public String headerByServlet(HttpServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("User-agent : " + request.getHeader("User-agent"));
+		sb.append(" || ");
+		sb.append("Practical-java : " + request.getHeader("Practical-java"));
+		return sb.toString();
+	}
+	@GetMapping (path = "/header-three")
+	public Map<String, String> headerByAll(@RequestHeader HttpHeaders httpHeaders) {
+		return httpHeaders.toSingleValueMap();
+	}
 }
